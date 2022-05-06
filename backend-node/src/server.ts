5fc6a63e7cf373bc6d909d25773ef39bb45b1761
josedoce <josesamya@gmail>
2022-05-06 14:09:05 -0300

    port config

--- backend-node/src/server.ts
+++ backend-node/src/server.ts
@@ -2,14 +2,12 @@ import express from 'express';
 import { routes } from './routes';
 import cors from 'cors';
 
-const PORT: number = 3333 || process.env.PORT;
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(PORT, ()=>{
-  console.info(`running on port ${PORT}`)
+app.listen(process.env.PORT || 3333, ()=>{
+  console.info(`running on port ${process.env.PORT}`)
 });
